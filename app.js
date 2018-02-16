3ea231a57f103240ad98839efa46dbf5dcdf234b
pusulurm <pusuluri.murali@gmail.com>
2018-02-16 21:13:07 +0800

    added express to serve application for production build

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var app = express();
 var path = require('path');
 
 
-app.use(express.static('client'))
+//app.use(express.static('clinet'))
+console.log(path.join(__dirname , 'client'));
+app.use(express.static(path.join(__dirname, 'client')));
+
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
+    res.sendFile(path.join(__dirname , '/client/index.html'));
 });
 
 app.listen(8080);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-var path    = require('path');
+var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   devtool: 'source-map',
   entry: {},
   module: {
     loaders: [
-       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
-       { test: /\.html$/, loader: 'raw' },
-       { test: /\.(scss|sass)$/, loader: 'style!css!sass' },
-       { test: /\.css$/, loader: 'style!css' }
+      { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.(scss|sass)$/, loader: 'style!css!sass' },
+      { test: /\.css$/, loader: 'style!css' }
     ]
   },
   plugins: [
@@ -30,6 +31,9 @@ module.exports = {
       minChunks: function (module, count) {
         return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
       }
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, 'app.js'), to: path.resolve(__dirname, 'dist') }
+    ])
   ]
 };
