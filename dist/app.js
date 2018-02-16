151c5d30e328f6c34c6c45c2ccc5691126486110
pusulurm <pusuluri.murali@gmail.com>
2018-02-17 00:00:06 +0800

    changed server app.js path

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+//app.use(express.static('clinet'))
+console.log(path.join(__dirname , 'client'));
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname , '/client/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
